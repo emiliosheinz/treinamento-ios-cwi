ec98027d352ad2406a0d767f071eb8d668e54fd6
emilioheinz <emiliosheinz@gmail.com>
2020-10-02 20:42:05 -0300

    Hello World

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Emilio Heinzmann on 02/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
