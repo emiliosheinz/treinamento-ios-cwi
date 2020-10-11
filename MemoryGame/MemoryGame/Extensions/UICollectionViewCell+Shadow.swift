bd41fa2efab1b4fc5a7e0b296c6bcaebb6c87640
emilioheinz <emiliosheinz@gmail.com>
2020-10-11 08:45:56 -0300

    feat(memory-game): fix shadows on cards

--- MemoryGame/MemoryGame/Extensions/UICollectionViewCell+Shadow.swift
+++ MemoryGame/MemoryGame/Extensions/UICollectionViewCell+Shadow.swift
@@ -10,8 +10,6 @@ import UIKit
 extension UICollectionViewCell {
     var withShadow: UICollectionViewCell {
         self.layer.cornerRadius = 10
-        self.layer.borderWidth = 1.0
-        self.layer.borderColor = UIColor.lightGray.cgColor
         self.layer.backgroundColor = UIColor.white.cgColor
         self.layer.shadowColor = UIColor.gray.cgColor
         self.layer.shadowOffset = CGSize(width: 2.0, height: 4.0)
