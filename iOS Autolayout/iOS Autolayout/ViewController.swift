34cd1998953781cacf8107d2afd0303cd38b76f1
emilioheinz <emiliosheinz@gmail.com>
2020-10-20 16:33:32 -0300

    feat: adding capsulas list

---
+++ iOS Autolayout/iOS Autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Autolayout
+//
+//  Created by Emilio Heinzmann on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
