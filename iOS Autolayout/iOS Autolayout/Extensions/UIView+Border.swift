6d4f53f8bc5ab76d9cb5ae23f0653456225c5107
emilioheinz <emiliosheinz@gmail.com>
2020-10-21 21:17:15 -0300

    feat: first auto layout constraints

---
+++ iOS Autolayout/iOS Autolayout/Extensions/UIView+Border.swift
@@ -0,0 +1,37 @@
+//
+//  UIView+Border.swift
+//  iOS Autolayout
+//
+//  Created by Emilio Heinzmann on 21/10/20.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
